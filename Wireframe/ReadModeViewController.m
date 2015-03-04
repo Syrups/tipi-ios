2117d04629a94241b15f2c8c122e52a0bc2522a7
Glenn Sonna <kenpachi.os@gmail.com>
2015-03-05 00:59:57 +0100

    relou avec bug

---
+++ Wireframe/ReadModeViewController.m
@@ -0,0 +1,37 @@
+//
+//  ReadModeViewController.m
+//  Wireframe
+//
+//  Created by Glenn Sonna on 05/03/2015.
+//  Copyright (c) 2015 Syrup Apps. All rights reserved.
+//
+
+#import "ReadModeViewController.h"
+
+@interface ReadModeViewController ()
+
+@end
+
+@implementation ReadModeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
