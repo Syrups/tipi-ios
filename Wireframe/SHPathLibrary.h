fb8275ee6364a9c72bc2089b708533d1d2077d67
Glenn Sonna <kenpachi.os@gmail.com>
2015-04-15 14:04:33 +0200

    added path lib file

---
+++ Wireframe/SHPathLibrary.h
@@ -0,0 +1,13 @@
+//
+//  SHPathLibrary.h
+//  Wireframe
+//
+//  Created by Glenn Sonna on 15/04/2015.
+//  Copyright (c) 2015 Syrup Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SHPathLibrary : NSObject
+
+@end
